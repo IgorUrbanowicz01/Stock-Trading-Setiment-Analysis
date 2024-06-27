0942ffe8d6af2f692edda8b065b8e657bc7481fd
Igor <urbanowiczigor95@gmail.com>
2024-06-27 16:32:50 +0200

    ssl certificate problems

--- plot.py
+++ plot.py
@@ -1,29 +1,38 @@
 
 import plotly.graph_objects as go
+import plotly.express as px
 from dash import Dash, dcc, html, Input, Output, callback
 def create_plot(hist_dict):
     app = Dash()
-    app.layout = [html.H2(children="Stocks and Sentiment"),
+    app.layout = html.Div([
+        html.H2(id='title-stock'),
                   dcc.Dropdown(
                       list(hist_dict.keys()),
                       list(hist_dict)[0],
                       id='dropdown-selection'
                   ),
-                  dcc.Graph(id = 'graph-contnt')
-                  ]
+                  dcc.Graph(id='graph-contnt')
+                  ])
     @callback(
         Output('graph-contnt', 'figure'),
         Input('dropdown-selection', 'value')
     )
     def update_graph_content(selected_value):
         df = hist_dict[selected_value]
-        return go.Figure(data=[go.Candlestick(x=df.index,
+        fig = go.Figure(data=[go.Candlestick(x=df.index,
                                open=df['Open'],
                                high=df['High'],
                                low=df['Low'],
-                               close=df['Close'])])
-
-
-
+                               close=df['Close'])],
+                        layout={'margin':dict(l=40, r=20, t=60, b=20),
+                                'height' : 500,
+                                'paper_bgcolor': 'LightSteelBlue'})
+        fig.update_layout()
+        return fig
+    @callback(
+        Output('title-stock', 'children'),
+        Input('dropdown-selection', 'value')
+    )
+    def update_title(selected_value):
+        return str(selected_value) + ' stock and public opinion'
     return app
-
